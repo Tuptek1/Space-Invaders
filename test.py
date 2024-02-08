b2237e96413e1c7c220c73e652de41b5b21ff67b
Tuptek1 <tuptek112@gmail.com>
2024-02-08 20:44:58 +0100

    started refractoring, created Friendly_Spaceship, Enemy, and Bullet classes

--- test.py
+++ test.py
@@ -5,46 +5,68 @@ pygame.init()
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 BLACK = (0, 0, 0)
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-clock = pygame.time.Clock()
+SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Space Invaders")
+CLOCK = pygame.time.Clock()
 running = True
-dt = 0
-
+DT = 0
 VEL = 1100
-BORDER = pygame.Rect(0, 0, SCREEN_WIDTH, SCREEN_WIDTH)
+FPS = 144
 
-player_pos = pygame.Vector2(SCREEN_WIDTH / 2 - 100, SCREEN_HEIGHT - 200)
 
+# will be changed to a pixel art background
+SCREEN.fill("black")
+
+
+class Bullet:
+    pass
+
+
+class Enemy:
+    pass
+
+    def draw_enemies():
+        pass
+
+
+class Friendly_Spaceship:
+    def __init__(self):
+        self.player_pos = pygame.Vector2(SCREEN_WIDTH / 2 - 100, SCREEN_HEIGHT - 200)
+        self.imp = pygame.image.load(
+            "./dd4af67c-c3f1-434e-9dad-659ef705893e.png"
+        ).convert()
+        self.imp = pygame.transform.scale(self.imp, (200, 200))
+
+    def handle_movement(self, keys, player_pos):
+        if keys[pygame.K_w] and player_pos.y > (SCREEN_HEIGHT * 2 / 3):
+            player_pos.y -= VEL * DT
+        if keys[pygame.K_s] and player_pos.y < (SCREEN_HEIGHT - 200):
+            player_pos.y += VEL * DT
+        if keys[pygame.K_a] and player_pos.x > 0:
+            player_pos.x -= VEL * DT
+        if keys[pygame.K_d] and player_pos.x < (SCREEN_WIDTH - 200):
+            player_pos.x += VEL * DT
+
+    def draw_friendly_spaceship(self, imp, player_pos):
+        SCREEN.blit(imp, (player_pos, imp.get_size()))
+
+
+friendly_spaceship = Friendly_Spaceship()
 while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
-
-    imp = pygame.image.load("./dd4af67c-c3f1-434e-9dad-659ef705893e.png").convert()
-    imp = pygame.transform.scale(imp, (200, 200))
-
-    screen.blit(imp, (player_pos, imp.get_size()))
+    # DT is delta time in seconds since last frame, used for framerate independent physics.
 
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_w] and player_pos.y > (SCREEN_HEIGHT * 2 / 3):
-        player_pos.y -= VEL * dt
-    if keys[pygame.K_s] and player_pos.y < (SCREEN_HEIGHT - 200):
-        player_pos.y += VEL * dt
-    if keys[pygame.K_a] and player_pos.x > 0:
-        player_pos.x -= VEL * dt
-    if keys[pygame.K_d] and player_pos.x < (SCREEN_WIDTH - 200):
-        player_pos.x += VEL * dt
+    friendly_spaceship.handle_movement(keys, friendly_spaceship.player_pos)
 
+    friendly_spaceship.draw_friendly_spaceship(
+        friendly_spaceship.imp, friendly_spaceship.player_pos
+    )
     pygame.display.update()
 
-    # limits FPS to 60
-    # dt is delta time in seconds since last frame, used for framerate-
-    # independent physics.
-    dt = clock.tick(144) / 1000
+    DT = CLOCK.tick(144) / 1000
 
 pygame.quit()
