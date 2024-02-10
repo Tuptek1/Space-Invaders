d3de97904e22a512609ebddb6967977d4bc541d3
choeqq <kacper.chola01@gmail.com>
2024-02-10 21:34:39 +0100

    scoreboard class

---
+++ scoreboard.py
@@ -0,0 +1,12 @@
+class Scoreboard: 
+    def __init__(self):
+        self.score = 0
+
+    def increase_score(self, points):
+        self.score += points
+
+    def reset_score(self):
+        self.score = 0
+
+    def get_score(self):
+        return self.score
