b4ae7d3277badf5aeb1862e8aed75c823a5d50cb
Tuptek1 <tuptek112@gmail.com>
2024-02-16 19:20:49 +0100

    further bullet logic

--- bullet.py
+++ bullet.py
@@ -1,25 +1,24 @@
 import pygame
+import time
 
 WHITE = (255, 255, 255)
+RED = (255, 87, 51)
+BLUE = (51, 131, 255)
 
 
 class Bullet(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.Surface((5, 10))
-        self.image.fill(WHITE)
-        self.bullet_pos = self.image.get_rect()
-        self.speed = -1
+        self.velocity = -7
         self.bullets_list = []
 
-    def create_bullet(self, bullets, player_pos):
-        self.bullet_pos.x = player_pos.x + 100
-        self.bullet_pos.y = player_pos.y
-        bullet = self.image
-        bullets.append(bullet)
+    def handle_bullet(self, bullets):
+        for bullet in bullets:
+            bullet.y += self.velocity
+            if bullet.y < 0:
+                bullets.remove(bullet)
+            # print(bullet)
 
-    def draw_bullet(self, screen, bullet):
-        if self.bullet_pos.y > 0:
-            self.bullet_pos.y += self.speed
-        screen.blit(bullet, self.bullet_pos)
-        print(self.bullet_pos)
+    def draw_bullet(self, screen, bullets):
+        for bullet in bullets:
+            pygame.draw.rect(screen, BLUE, bullet)
